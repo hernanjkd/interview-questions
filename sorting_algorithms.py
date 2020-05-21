c876db470cd1cda6b87a736e3b4ab15d2ba498e6
hernanjkd <hernanjkd@gmail.com>
2020-05-21 00:52:31 -0400

    time and compare sortings

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,6 +1,24 @@
 from time import time
 from random import randrange, randint
 
+# lst length 10
+# inser 1.3074874877929688e-05
+# bubbl 2.1622180938720703e-05
+# quick 4.0788650512695315e-05
+# merge 5.470514297485351e-05
+
+# lst length 100
+# bubbl 4.2753219604492186e-05
+# inser 4.7523975372314456e-05
+# quick 0.0005417561531066894
+# merge 0.0010808634757995606
+
+# lst length 10,000
+# quick 0.06157848358154297
+# inser 0.1027791714668274
+# merge 0.16664746761322022
+# bubbl 0.23856120586395263
+
 length = 100
 lst = [randrange(length) for x in range(length)]
 
@@ -10,7 +28,7 @@ lst = [randrange(length) for x in range(length)]
 # already_sorted flag kicks in.
 # Takes the largest elements to the right on every iteration, stopping one item
 # sooner every time
-def bubble_sort(lst):
+def bubblesort(lst):
     n = len(lst)
     for i in range(n):
         already_sorted = True
@@ -21,7 +39,7 @@ def bubble_sort(lst):
         if already_sorted:
             break
     return lst
-# print(bubble_sort(lst))
+
 
 
 # INSERTION SORT (time complexity: quadratic, linear)
@@ -31,7 +49,7 @@ def bubble_sort(lst):
 # to do less comparisons.
 # Takes in each element from left to right, and inserts it left of the element
 # starting right to left until it finds a smaller element
-def insertion_sort(lst):
+def insertionsort(lst):
     for i in range(1, len(lst)):
         item = lst[i]
         j = i-1
@@ -40,7 +58,7 @@ def insertion_sort(lst):
             j -= 1
         lst[j+1] = item
     return lst
-# print(insertion_sort(lst))
+
 
 
 # MERGE SORT (time complexity: logarithmic, for the merge func it's linear)
@@ -69,22 +87,22 @@ def merge(left, right):
             break
     return result
 
-def merge_sort(lst):
+def mergesort(lst):
     if len(lst) < 2:
         return lst
     midpoint = len(lst)//2
     return merge(
-        left = merge_sort(lst[:midpoint]),
-        right= merge_sort(lst[midpoint:])
+        left = mergesort(lst[:midpoint]),
+        right= mergesort(lst[midpoint:])
     )
-# print(merge_sort(lst))
+
 
 
 # QUICKSORT (time complexity: logarithmic)
 # Divide and conquer. Divides the list into smaller same and larger elements
 # comparing them to a pivot element chosen at random. It recursively keeps
 # dividing the list and combining it until it's sorted
-def quick_sort(lst):
+def quicksort(lst):
     if len(lst) < 2:
         return lst
     low, same, high = [], [], []
@@ -96,5 +114,21 @@ def quick_sort(lst):
             high.append(x)
         elif x == pivot:
             same.append(x)
-    return quick_sort(low) + same + quick_sort(high)
-# print(quick_sort(lst))
+    return quicksort(low) + same + quicksort(high)
+
+
+
+
+def timesort(func, lst):
+    times = []
+    for x in range(100):
+        arr = [*lst]
+        t = time()
+        func(arr)
+        times.append(time()-t)
+    return sum(times)/100
+
+print('bubbl',timesort(bubblesort, lst))
+print('inser',timesort(insertionsort, lst))
+print('merge',timesort(mergesort, lst))
+print('quick',timesort(quicksort, lst))
