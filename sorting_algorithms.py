c9e22decc088769f5b920a53e2ba790ef441ca62
hernanjkd <hernanjkd@gmail.com>
2020-05-20 23:04:13 -0400

    quick sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,15 +1,15 @@
 from time import time
-from random import randrange
+from random import randrange, randint
 
 length = 100
 lst = [randrange(length) for x in range(length)]
 
 
 # BUBBLE SORT (time complexity: quadratic, linear)
-# Time complexity is linear if when it goes thru it it's already
-# sorted and the already_sorted flag kicks in.
-# Takes the largest elements to the right on every iteration,
-# stopping one item sooner every time
+# Time complexity is linear if when it goes thru it it's already sorted and the
+# already_sorted flag kicks in.
+# Takes the largest elements to the right on every iteration, stopping one item
+# sooner every time
 def bubble_sort(lst):
     n = len(lst)
     for i in range(n):
@@ -27,11 +27,10 @@ def bubble_sort(lst):
 # INSERTION SORT (time complexity: quadratic, linear)
 # Best for small lists.
 # Time complexity is linear when the list is already sorted.
-# Insertion sort is more efficient than bubble sort because in
-# general it has to do less comparisons.
-# Takes in each element from left to right, and inserts it
-# left of the element starting right to left until it finds
-# a smaller element
+# Insertion sort is more efficient than bubble sort because in general it has 
+# to do less comparisons.
+# Takes in each element from left to right, and inserts it left of the element
+# starting right to left until it finds a smaller element
 def insertion_sort(lst):
     for i in range(1, len(lst)):
         item = lst[i]
@@ -44,8 +43,10 @@ def insertion_sort(lst):
 # print(insertion_sort(lst))
 
 
-# MERGE SORT
-# Divide and conquer
+# MERGE SORT (time complexity: logarithmic, for the merge func it's linear)
+# Divide and conquer. Very efficient algorithm, faster for big lists,
+# slower than bubble and insertion for small lists. Takes up too much mem.
+# Halves the list until there's only one item, then merges all the lists.
 def merge(left, right):
     if len(left) == 0:
         return right
@@ -76,4 +77,24 @@ def merge_sort(lst):
         left = merge_sort(lst[:midpoint]),
         right= merge_sort(lst[midpoint:])
     )
-print(merge_sort(lst))
+# print(merge_sort(lst))
+
+
+# QUICKSORT (time complexity: logarithmic)
+# Divide and conquer. Divides the list into smaller same and larger elements
+# comparing them to a pivot element chosen at random. It recursively keeps
+# dividing the list and combining it until it's sorted
+def quick_sort(lst):
+    if len(lst) < 2:
+        return lst
+    low, same, high = [], [], []
+    pivot = lst[ randint(0, len(lst)-1) ]
+    for x in lst:
+        if x < pivot:
+            low.append(x)
+        elif x > pivot:
+            high.append(x)
+        elif x == pivot:
+            same.append(x)
+    return quick_sort(low) + same + quick_sort(high)
+# print(quick_sort(lst))
