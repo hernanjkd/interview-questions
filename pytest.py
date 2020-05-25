4e3dd8d4e550aca4590064060e85c593c42ab72c
hernanjkd <hernanjkd@gmail.com>
2020-05-24 23:17:27 -0400

    testing

---
+++ pytest.py
@@ -0,0 +1,14 @@
+import os, re
+
+# print('file', __file__)
+# print('abspath', os.path.abspath(__file__))
+# print('dirname', os.path.dirname(os.path.abspath(__file__)))
+
+print( 0 < 5 < 20 < 15)
+
+
+
+
+# What is bc from bc.json?
+''' breathecode '''
+# exercise 15 has language ['us','us'] when I believe one should be 'es'
