f4b2e730bf391887a3354d29bb4153d6b7de041c
hernanjkd <hernanjkd@gmail.com>
2020-05-26 23:26:59 -0400

    mark.it

--- pytest.py
+++ pytest.py
@@ -1,10 +1,18 @@
-import os, re
+import pytest, io, sys, json
+
+@pytest.mark.it('Sum all three input numbers and print on the console the result')
+def test_add_variables(capsys):
+
+    fake_input = [2,3,4] #fake input
+    with mock.patch('builtins.input', lambda x: fake_input.pop()):
+      captured = capsys.readouterr()
+      assert captured.out == "9\n"
 
 # print('file', __file__)
 # print('abspath', os.path.abspath(__file__))
 # print('dirname', os.path.dirname(os.path.abspath(__file__)))
 
-print( 0 < 5 < 20 < 15)
+
 
 
 
