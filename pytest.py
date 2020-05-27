b3b09bdabf8f8d3af1b00dc14ff4dad8d0d8f193
hernanjkd <hernanjkd@gmail.com>
2020-05-26 23:43:32 -0400

    co

--- pytest.py
+++ pytest.py
@@ -3,11 +3,12 @@
 #Complete the funtion to compute how many seconds passed between the two timestamp.
 def two_timestamp(hr1,min1,sec1,hr2,min2,sec2):
     
-    return hr2*60*60 + min2*60 + sec2 - hr1*60*60 + min1*60 + sec1
+    return (hr2*60*60 + min2*60 + sec2) - (hr1*60*60 + min1*60 + sec1)
 
 
 #Invoke the fuction and pass two timestamps(6 intergers) as its argument.
 print(two_timestamp(1,1,1,2,2,2))
+print(two_timestamp(1,2,30,1,3,20))
 
 
 
