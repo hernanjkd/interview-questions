9d679fd6ff24231be10156b5aec8eac68e83c25c
hernanjkd <hernanjkd@gmail.com>
2020-05-27 20:01:17 -0400

    using the print func

--- pytest.py
+++ pytest.py
@@ -1,18 +1,10 @@
-
-
-#Complete the function to return the tens digit and the ones digit of any interger.
-def two_digits(digit):
-  return digit//10, digit%10
-   
-
-
-#Invoke the function with any interger as its argument.
-print(two_digits(79))
-
-
-
-
-
-# What is bc from bc.json?
-''' breathecode '''
 # exercise 15 has language ['us','us'] when I believe one should be 'es'
+
+# Using the print function
+
+@pytest.mark.it('Use the print function')
+def test_output():
+    f = open(os.path.dirname(os.path.abspath(__file__)) + '/app.py')
+    content = f.read()
+    assert content.find('print(') > 0
+
