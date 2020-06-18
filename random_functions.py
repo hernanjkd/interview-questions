228623f050d6dc99f61c1b140a0342449bef3244
hernanjkd <hernanjkd@gmail.com>
2020-06-17 23:57:57 -0400

    find max and min

--- random_functions.py
+++ random_functions.py
@@ -1,6 +1,6 @@
-# How do you remove duplicates from an array in place?
 lst = [1,2,3,3,4,5,6,7]
 
+# How do you remove duplicates from an array in place?
 def remove_duplicates(lst):
     track = []
     for i, e in enumerate(lst):
@@ -9,15 +9,21 @@ def remove_duplicates(lst):
         else:
             track.append(e)
     return lst
-
 # print( remove_duplicates(lst) )
+
 
 # How do you reverse an array in place?
 def reverse(lst):
     for i in range( len(lst) // 2 ):
         lst[i], lst[-(i+1)] = lst[-(i+1)], lst[i]
     return lst
+# print( reverse(lst) )
 
-print( reverse(lst) )
-# lst[1], lst[-2] = lst[-2], lst[1]
-# print(lst)
+
+# How do you find the largest and smallest number in an unsorted integer array?
+def find_max_min(lst):
+    return {
+        'smallest': min(lst),
+        'largest': max(lst)
+    }
+print( find_max_min(lst) )
