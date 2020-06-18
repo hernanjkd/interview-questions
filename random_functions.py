326016aebd9a6555a31983f0317582503d8fc287
hernanjkd <hernanjkd@gmail.com>
2020-06-17 23:52:16 -0400

    reverse in place

--- random_functions.py
+++ random_functions.py
@@ -1,5 +1,5 @@
 # How do you remove duplicates from an array in place?
-lst = [1,2,3,3,4,5,6]
+lst = [1,2,3,3,4,5,6,7]
 
 def remove_duplicates(lst):
     track = []
@@ -14,10 +14,10 @@ def remove_duplicates(lst):
 
 # How do you reverse an array in place?
 def reverse(lst):
-    for i in range( len(lst) / 2 ):
-        lst[i], lst[-i] = lst[-i], lst[i]
+    for i in range( len(lst) // 2 ):
+        lst[i], lst[-(i+1)] = lst[-(i+1)], lst[i]
     return lst
 
 print( reverse(lst) )
-# lst.append(lst.pop(0))
+# lst[1], lst[-2] = lst[-2], lst[1]
 # print(lst)
